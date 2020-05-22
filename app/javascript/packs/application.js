2e33f917cd565d9fca507afed56ba0e5c781d082
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2020-05-22 19:15:07 -0400

    Add functionality to dropdown menu

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,9 +7,11 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
+require("semantic-ui-sass")
 
-import "semantic-ui-sass"
-
+window.onload = function(){
+    $('.ui.dropdown').dropdown();
+};
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
