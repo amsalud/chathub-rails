2dbe7a05b1ed67b4bc57d0562578c9d54da130fb
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2020-05-23 00:32:00 -0400

    Add functionality to close message alerts

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,8 +9,11 @@ require("@rails/activestorage").start()
 require("channels")
 require("semantic-ui-sass")
 
-window.onload = function(){
+window.onload = function () {
     $('.ui.dropdown').dropdown();
+    $('.message .close').on('click', function () {
+        $(this).closest('.message').transition('fade');
+    });
 };
 
 // Uncomment to copy all static images under ../images to the output folder and reference
