d61fc18ef456534d5c8cd1dcbc627761e5528d27
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2020-05-23 16:45:26 -0400

    Broadcast new message partial to the chatroom channel when new message is sent by a user

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -3,14 +3,17 @@ class MessagesController < ApplicationController
 
   def create
     message = Message.create(body: message_params[:body], user: current_user)
-
     if message.save
-      redirect_to root_path    
+      ActionCable.server.broadcast "chatroom_channel", message_partial: render_message(message)  
     end
   end
 
   private 
   def message_params
     params.require(:message).permit(:body)
+  end
+
+  def render_message(message)
+    render(partial: 'message', locals: {message: message})
   end
 end
